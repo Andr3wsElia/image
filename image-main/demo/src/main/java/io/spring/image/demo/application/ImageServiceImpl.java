544397ebed105c3952300cd5d71c69989e64e8b1
Andr3wsElia <andrews.elia@estudante.ifmt.edu.br>
2026-05-15 12:02:33 -0400

    DTO e Mapper de Imagens
    15/05

--- image-main/demo/src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
+++ image-main/demo/src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
@@ -3,11 +3,13 @@ package io.spring.image.demo.application;
 
 import io.spring.image.demo.domain.entity.Image;
 import io.spring.image.demo.domain.service.ImageService;
+import io.spring.image.demo.enums.ImageExtension;
 import io.spring.image.demo.infra.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,7 +24,13 @@ public class ImageServiceImpl implements ImageService {
     public Image save(Image image){
         return repository.save(image);
     }
+
     public Optional<Image> getById(String id){
         return repository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtension extension, String query){
+        return repository.findByExtensionAndNameOrTagsLike(extension, query);
     }
 }
