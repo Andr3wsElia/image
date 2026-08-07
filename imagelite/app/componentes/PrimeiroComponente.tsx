6e935715e67fd903d9ee990904aece8d71cad491
AndrewsElia <andrews.elia@estudante.ifmt.edu.br>
2026-08-07 11:59:40 -0400

    07/08/2026

---
+++ imagelite/app/componentes/PrimeiroComponente.tsx
@@ -0,0 +1,7 @@
+export default function PrimeiroComponente() {
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+        </div>
+    );
+}
--- imagelite/app/page.tsx
+++ imagelite/app/page.tsx
@@ -4,7 +4,16 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
+        <h1 className="text-5xl font-bold text-black dark:text-white sm:text-6xl">
+          Welcome to{" "}
+          <a
+            href="https://nextjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Next.js!
+          </a>
+        </h1>
       </main>
     </div>
   );
