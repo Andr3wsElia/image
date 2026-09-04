4a1ae4874f6589d31ee9f44bbaa3bb57798a46de
AndrewsElia <andrews.elia@estudante.ifmt.edu.br>
2026-09-04 11:44:17 -0400

    04/09

--- imagelite/app/resource/service.ts
+++ imagelite/app/resource/service.ts
@@ -1,12 +1,12 @@
-import { Image } from "./image";
+import { Image } from "./image";  
 
-class ImageService {
-    baseURL: string = 'http://localhost:8080/v1/image';
-    
-    async buscar(): Promise<Image[]>{
-        const response = await fetch({this.baseURL});
-        return await response.json();
-    }
+export class ImageService {
+  baseURL: string = 'http://localhost:8080/images';
+
+  async buscar(): Promise<Image[]> {
+    const response = await fetch(this.baseURL);
+    return  await response.json();
+  }
 }
-
-export const useImage = () => new ImageService();
+// React Hook
+export const useImageService = () => new ImageService();
