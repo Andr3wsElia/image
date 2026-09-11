65647767346c571e2cb5f3fa59178ed4afef0e98
AndrewsElia <andrews.elia@estudante.ifmt.edu.br>
2026-09-11 12:01:21 -0400

    Imagens Carregadas no Site

--- imagelite/app/galeria/page.tsx
+++ imagelite/app/galeria/page.tsx
@@ -22,21 +22,39 @@ export default function Galeria() {
     console.table(result)
   }
 
-  
+  function renderImageCard(image: Image) {
+    return (
+      <ImageCard imageName = {image.name} 
+                 imageUrl = {image.url}
+                 imageSize = {image.size}
+                 uploadDate = {image.uploadDate}
+      />
+    )
+  }
+
+  function renderImageCards() {
+    return images.map(renderImageCard);
+  }
 
   return (
    
       <Template>
+
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={searchImages}>
           Mudar Imagem
         </button>
-          <section className="grid grid-cols-3 gap-4  p-4">
-            
-            <ImageCard  imageName='{images[0]?.name}'/>
-          </section>
+
+        <section className="grid grid-cols-3 gap-4  p-4">
+          <ImageCard  imageName='{images[0]?.name}'/>
           
-       
-        
+        </section>
+
+        <section>
+          {
+            renderImageCards()
+          }
+        </section>
+          
       </Template>
    
   )
