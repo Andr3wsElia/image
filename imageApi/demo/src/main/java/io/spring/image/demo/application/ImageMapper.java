a0deb95298a91a228980384410c9c625b57a5c17
AndrewsElia <andrews.elia@estudante.ifmt.edu.br>
2026-09-18 11:58:03 -0400

    Barra de Busca

--- imageApi/demo/src/main/java/io/spring/image/demo/application/ImageMapper.java
+++ imageApi/demo/src/main/java/io/spring/image/demo/application/ImageMapper.java
@@ -5,6 +5,8 @@ import io.spring.image.demo.domain.enums.ImageExtension;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
